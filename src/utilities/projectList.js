18340c1600598460752ed6a1ac3a42a36c2fb7d6
Steve Fox <stevefox.dev@gmail.com>
2025-04-28 11:15:22 -0400

    change some gif's to normal images, replaced gif's with smaller widths, adjusted project card hover to only on non touch screens, all to improve mobile performance

--- src/utilities/projectList.js
+++ src/utilities/projectList.js
@@ -9,6 +9,15 @@ const projectList = [
       image: 'quilt.gif',
   },
   {
+    title: 'Fayetteville',
+    info:
+      "This is a mock landing page for Fayetteville, WV. It is built using only HTML, CSS/SASS, and a few lines of JS to close the nav menu. The idea behind this was to just build something using the basics.",
+    link: 'https://fayetteville-landing-page.vercel.app/',
+    code: 'https://github.com/SteveFoxDev/Fayetteville_Landing_Page',
+    skills: 'HTML | CSS | SASS | JavaScript',
+    image: 'fayette.png',
+  },
+  {
     title: 'miniSplash',
     info:
       "miniSplash is a miniature version of Unsplash built in ReactJS, using the Unsplash API. It features infinite scroll, and mimics the Unsplash dense grid system.",
@@ -18,13 +27,13 @@ const projectList = [
     image: 'miniSplash.gif',
   },
   {
-    title: 'Fayetteville',
+    title: 'mAPI',
     info:
-      "This is a mock landing page for Fayetteville, WV. It is built using only HTML, CSS/SASS, and a few lines of JS to close the nav menu. The idea behind this was to just build something using the basics.",
-    link: 'https://fayetteville-landing-page.vercel.app/',
-    code: 'https://github.com/SteveFoxDev/Fayetteville_Landing_Page',
-    skills: 'HTML | CSS | SASS | JavaScript',
-    image: 'fayette.gif',
+      "mAPI is a miniature map app built in ReactJS, using the openStreetMaps API to return geolocation data from location searches, and uses MapBox to display the map.",
+    link: 'https://mapi-zeta.vercel.app/',
+    code: 'https://github.com/SteveFoxDev/mAPI',
+    skills: 'ReactJS | JavaScript | NodeJS | SASS | axios',
+    image: 'mapi.png',
   },
   {
     title: 'To-Be-Read',
@@ -36,22 +45,13 @@ const projectList = [
       image: 'ToBeRead.gif',
   },
   {
-    title: 'mAPI',
-    info:
-      "mAPI is a miniature map app built in ReactJS, using the openStreetMaps API to return geolocation data from location searches, and uses MapBox to display the map.",
-    link: 'https://mapi-zeta.vercel.app/',
-    code: 'https://github.com/SteveFoxDev/mAPI',
-    skills: 'ReactJS | JavaScript | NodeJS | SASS | axios',
-    image: 'mapi.gif',
-  },
-  {
     title: 'YelpCamp',
     info:
       "YelpCamp was built as part of a project during the Web Developer Bootcamp. I add features to it when I learn new technologies, and use it as a reference for auth, routing, middleware etc.",
     link: 'https://frozen-mountain-78723-f8a2d977ccdb.herokuapp.com/',
     code: 'https://github.com/SteveFoxDev/YelpCamp',
     skills: 'NodeJS | Express | MongoDB | EJS | JavaScript | HTML | Bootstrap',
-    image: 'yelp.gif',
+    image: 'yelp.png',
     note: 'YelpCamp is served on a student tier Heroku server, it may take a few moments to wake the server.'
   },
   ];
